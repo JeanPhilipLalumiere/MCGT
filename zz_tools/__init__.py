2241491bd7a512563726dc60bf3c4a9eba110396
Jean-Philip Lalumiere <contact@jeanphiliplalumiere.com>
2025-10-20 20:20:41 -0400

    release: zz-tools 0.2.99 (classifiers, bump, housekeeping)

--- zz_tools/__init__.py
+++ zz_tools/__init__.py
@@ -1,7 +1,7 @@
 try:
     from importlib.metadata import version, PackageNotFoundError
-    __version__ = version("zz-tools")
+    __version__ = "0.2.99"
 except Exception:
-    __version__ = "0+unknown"
+    __version__ = "0.2.99"
 # exports publics éventuels
 from .common_io import *  # si nécessaire pour ton API

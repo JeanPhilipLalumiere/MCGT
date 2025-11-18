5e7fb5563b7d634722e9607ce6b130b706179291
Jean-Philip Lalumiere <contact@jeanphiliplalumiere.com>
2025-11-18 16:35:31 -0500

    release(zz-tools): bump to 0.3.1.post1

--- zz_tools/__init__.py
+++ zz_tools/__init__.py
@@ -1,7 +1,7 @@
 try:
     from importlib.metadata import version, PackageNotFoundError
-    __version__ = "0.2.99"
+    __version__ = "0.3.1.post1"
 except Exception:
-    __version__ = "0.2.99"
+    __version__ = "0.3.1.post1"
 # exports publics éventuels
 from .common_io import *  # si nécessaire pour ton API

d2a63dc68f10fac2cd2750aadc572aaf532ed2c2
Jean-Philip Lalumiere <contact@jeanphiliplalumiere.com>
2025-11-28 15:20:03 -0500

    chore: release 0.3.11

--- zz_tools/__init__.py
+++ zz_tools/__init__.py
@@ -1,7 +1,7 @@
 try:
     from importlib.metadata import version, PackageNotFoundError
-    __version__ = "0.3.1.post1"
+    __version__ = "0.3.11"
 except Exception:
-    __version__ = "0.3.1.post1"
+    __version__ = "0.3.11"
 # exports publics éventuels
 from .common_io import *  # si nécessaire pour ton API

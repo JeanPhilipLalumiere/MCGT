b62cb4205ab0c25a389b23e34b6c7ce2d5aa0f49
Jean-Philip Lalumiere <contact@jeanphiliplalumiere.com>
2025-12-19 17:44:16 -0500

    fix(ci): remove tracked plot_ artifacts from index

--- fix_plot_fig04_cli_seed.sh
+++
@@ -1,6 +0,0 @@
-# 1) Copier le script téléchargé dans le repo (ex: ~/MCGT)
-# 2) Lancer le patch :
-bash fix_plot_fig04_cli_seed.sh .
-
-# 3) (Optionnel) Relancer ta vérif locale si tu as le script :
-./mcgt_verify_round3_step2.sh
